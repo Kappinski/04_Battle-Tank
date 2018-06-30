8ba6d86c310f247b9435b86756fd15bb697dcb17
Kappinski <capsplay25@gmail.com>
2018-06-30 13:54:23 +0200

    BT09 Player and AI Controller, C++ classes

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -2,31 +2,20 @@
 
 #include "TankAIController.h"
 #include "Engine/World.h"
-#include "Engine/StaticMesh.h"
 #include "GameFramework/Actor.h"
 
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	auto ControlledTank = GetControlledTank();
-	if (!ControlledTank)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("TankAIController not possesing a tank"));
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("TankAIController possesing: %s"), *(ControlledTank->GetName()));
-	}
-
 	auto PlayerTank = GetPlayerTank();
 	if (!PlayerTank)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Couldn't find PlayerTank"));
+		UE_LOG(LogTemp, Warning, TEXT("AIController can't find player tank"));
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("PlayerTank found: %s"), *(PlayerTank->GetName()));
+		UE_LOG(LogTemp, Warning, TEXT("AIController found player: %s"), *(PlayerTank->GetName()));
 	}
 
 }
@@ -38,7 +27,7 @@ ATank * ATankAIController::GetControlledTank() const
 
 ATank* ATankAIController::GetPlayerTank() const
 {
-	auto PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
 
 	if (!PlayerPawn) { return nullptr; }
 
--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -18,8 +18,23 @@ void ATankPlayerController::BeginPlay()
 
 }
 
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick( DeltaTime );
+	//AimTowardsCrosshair();
+	//UE_LOG(LogTemp, Warning, TEXT("PlayerController ticking"));
+}
+
 ATank * ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
 
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	// get world location if linetrace through crosshair
+	// If it hits the landscape
+		// Tell controlled tank to aim at this point
+}
